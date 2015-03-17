e52e245cfd82ed6bd29b15cc3bcba2260335a30c
Michael Clark <kovels@hotmail.com>
2015-03-17 17:43:19 -0400

    Initial commit

---
+++ MovieCatalogLibrary/ImageHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieCatalogLibrary
+{
+    public class ImageHandler
+    {
+
+        /// <summary>
+        /// Async method for getting the IMG bytes file.
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public async Task<byte[]> getContentAsync(string imageUrl)
+        {
+            var content = new MemoryStream();
+
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(imageUrl);
+
+            using (WebResponse response = await webRequest.GetResponseAsync())
+            {
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    await responseStream.CopyToAsync(content);
+                }
+            }
+
+            return content.ToArray();
+        }
+    }
+}
---
+++ MovieCatalogLibrary/TMDBHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WatTmdb.V3;
+
+namespace MovieCatalogLibrary
+{
+    public class TMDBHelper
+    {
+        private static readonly string apikey = "56587e13dc926d742e62c09151418bd3";
+        private static readonly string language = "en";
+        private static Tmdb api = new Tmdb(apikey, language);
+
+        public List<MovieResult> movieResultsBySearch(string searchTerm)
+        {
+            if (searchTerm.Count() != 0)
+            {
+                TmdbMovieSearch results = api.SearchMovie(searchTerm, 1);
+                return (api.SearchMovie(searchTerm, 1) as TmdbMovieSearch).results;
+            }
+
+            else
+            {
+                return new List<MovieResult>();
+            }
+        }
+
+        public TmdbMovie getTmdbMovieById(int mid)
+        {
+            return api.GetMovieInfo(mid);
+        }
+
+        public TmdbMovieImages getImagesById(int mid)
+        {
+            return api.GetMovieImages(mid);
+        }
+    }
+}
